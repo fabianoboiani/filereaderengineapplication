d5bd417da3a3c0c60ff689e6f0548d7354599fc8
Fabiano Boiani <fabianoboiani@gmail.com>
2018-05-12 11:46:11 -0300

    1

--- src/main/java/br/com/prova/filereaderengine/GeradorRelatorio.java
+++ src/main/java/br/com/prova/filereaderengine/GeradorRelatorio.java
@@ -64,7 +64,7 @@ public class GeradorRelatorio {
 				for (Item item : venda.getItemVendidos()) {
 					valorDaVenda = valorDaVenda.add(item.getPreco().multiply(BigDecimal.valueOf(item.getQuantidade())));
 				}
-				System.out.println(venda.getNome()+":"+valorDaVenda);
+				//System.out.println(venda.getNome()+":"+valorDaVenda);
 				if (menorValor == null) {
 					menorValor = valorDaVenda;
 					piorVendedor = venda.getNome();
@@ -88,7 +88,7 @@ public class GeradorRelatorio {
 				for (Item item : venda.getItemVendidos()) {
 					valorDaVenda = valorDaVenda.add(item.getPreco().multiply(BigDecimal.valueOf(item.getQuantidade())));
 				}
-				System.out.println(venda.getIdVenda()+":"+valorDaVenda);
+				//System.out.println(venda.getIdVenda()+":"+valorDaVenda);
 				if (valorDaVenda.compareTo(maiorValor) > 0) {
 					idVendaMaisCara = venda.getIdVenda();
 					maiorValor = valorDaVenda;
